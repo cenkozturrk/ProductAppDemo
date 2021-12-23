200a906b0bd2fb85266a1f63497241c75a850d56
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-23 15:51:45 +0300

    IProductRepository was created. In order not to write the same repository codes over and over, an interface named IGenericRepository was created. IProductRepository was pulled into IGenericRepository by making Inherid.

--- src/Core/ProductApp.Application/Interfaces/IProductRepository.cs
+++ src/Core/ProductApp.Application/Interfaces/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using ProductApp.Domain.Entities;
+﻿using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,10 +7,8 @@ using System.Threading.Tasks;
 
 namespace ProductApp.Application.Interfaces
 {
-    public interface IProductRepository
+    public interface IProductRepository: IGenericRepositoryAsync<Product>
     {
-        Task<List<Product>> GetAllProduct();
-        Task<Product> GetProductById(Guid Id);
-
+        //ById methodları süreklü bütün repositorylerin içinde olucak.Bu yüzden her repository içine aynı kodları yazmak yerine GenericRepository yazabiliriz.     
     }
 }
---
+++ src/Core/ProductApp.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using ProductApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Interfaces.Repository
+{
+    //T alacak Generic olacağı için.  
+    public interface IGenericRepositoryAsync<T> where  T : BaseEntity //Where kullanarak T gönderilecek şeyi entity olarak kısıkladık(BaseEntity harici birşey gönderilemez).
+    {
+        //ById methodları süreklü bütün repositorylerin içinde olucak.Bu yüzden her repository içine aynı kodları yazmak yerine GenericRepository yazabiliriz.
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid Id);
+        Task<T> AddAsync(T entity); //Entity gönderildiğinde bu method ile entitileri alıyoruz.
+
+    }
+}
