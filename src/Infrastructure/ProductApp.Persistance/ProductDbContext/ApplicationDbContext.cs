195d38c757a5284236e05a4d8a3d8299026f4c7e
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-23 17:16:54 +0300

    DbContext file was created. Since we do not use SQL, the data was processed there by writing the modelbuilder method. The data will come from there every time the application is opened.

---
+++ src/Infrastructure/ProductApp.Persistance/ProductDbContext/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp.Persistance.ProductDbContext
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Uygulama her çalıştığında sql kullanmadığımız için bu method ile aşağıdaki veriler gelicek.
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { Id = Guid.NewGuid(), Name = "Pen", Value = 10, Quantity = 100 },
+                new Product() { Id = Guid.NewGuid(), Name = "Paper A4", Value = 1, Quantity = 200 },
+                new Product() { Id = Guid.NewGuid(), Name = "Book", Value = 30, Quantity = 500 }); ;
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
