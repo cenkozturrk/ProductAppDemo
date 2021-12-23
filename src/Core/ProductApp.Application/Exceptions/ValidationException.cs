f42596c9b862e2c88356beeeba2f152f04a4d2d3
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-23 15:10:49 +0300

    Dto and exceptions files added.

---
+++ src/Core/ProductApp.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp.Application.Exceptions
+{
+    public class ValidationException: Exception
+    {
+        public ValidationException() : this("Validation error occured")
+        {
+
+        }
+        public ValidationException(String Message): base(Message)
+        {
+
+        }
+        public ValidationException(Exception ex): this(ex.Message)
+        {
+
+        }
+
+    }
+}
