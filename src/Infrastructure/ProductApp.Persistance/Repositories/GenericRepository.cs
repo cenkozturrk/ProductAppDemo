c8b6e3df66a6f29bcadeadcbdf6c3336dbf56056
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-23 17:24:35 +0300

    A repository file was created inside the persistence method and it was implemented using dependency injection.

---
+++ src/Infrastructure/ProductApp.Persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Domain.Common;
+using ProductApp.Persistance.ProductDbContext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Persistance.Repositories
+{
+    public class GenericRepository<T> : IGenericRepositoryAsync<T> where T: BaseEntity
+    {
+
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+
+        }
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Infrastructure/ProductApp.Persistance/Repositories/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProductApp.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp.Persistance.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+
+    }
+}
