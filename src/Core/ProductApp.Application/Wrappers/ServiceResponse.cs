d3278d5379a286b0f1bb7946ba049b1ad2480a27
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-23 15:24:55 +0300

    wrappers file added (Response classes created.)

---
+++ src/Core/ProductApp.Application/Wrappers/ServiceResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp.Application.Wrappers
+{
+    public class ServiceResponse<T>
+    {
+        public T Value { get; set; }
+
+        //kendi value mizi direk dısardan gelen value olarak set edebiliriz.
+        public ServiceResponse(T value)
+        {
+            Value = value;
+        }
+
+        public ServiceResponse()
+        {
+
+        }
+    }
+}
